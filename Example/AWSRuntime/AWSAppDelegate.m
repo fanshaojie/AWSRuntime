9516eb507eab837e37c4e0efa80ddb2666665588
wanghuizhou <wanghuizhou@guazi.com>
2018-12-25 11:43:24 +0800

    。。

--- Example/AWSRuntime/AWSAppDelegate.m
+++ Example/AWSRuntime/AWSAppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // 拿本
     // Override point for customization after application launch.
     return YES;
 }
